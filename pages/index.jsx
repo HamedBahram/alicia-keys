35eecc6f95cf5df5b1cfae5ac9ceb60f4290e10a
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2023-03-28 12:32:18 -0400

    Initial setup with Tailwind

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>Home page</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Home
