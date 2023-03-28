5d9add081020336207902fee9298d2b4f6a4e328
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2023-03-28 13:15:06 -0400

    Initial prismic setup

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../prismicio'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicProvider internalLinkComponent={props => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  )
 }
