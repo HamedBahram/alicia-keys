d2587f82592947b7e14c16c59db8b78bb5215e01
Alex Trost <19617280+a-trost@users.noreply.github.com>
2023-03-28 20:17:43 -0400

    Add scroll effects to media slices

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,50 @@
+import { useRef, useEffect } from 'react'
 import { SliceZone } from '@prismicio/react'
+import { gsap } from 'gsap/dist/gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 
 import { createClient } from '../prismicio'
 import { components } from '../slices'
 
+gsap.registerPlugin(ScrollTrigger)
+
 export default function Page({ page }) {
-  return <SliceZone slices={page.data.slices} components={components} />
+  const main = useRef(null)
+
+  useEffect(() => {
+    const ctx = gsap.context(self => {
+      const boxes = self.selector('.single-media-highlight')
+      boxes.forEach((box, i) => {
+        gsap.set(box, {
+          zIndex: i,
+          position: 'sticky',
+
+          transformOrigin: 'center bottom'
+        })
+        gsap.to(box, {
+          '--blurIntensity': '50px',
+          y: '100vh',
+          bottom: 0,
+          ease: 'none',
+          duration: 3,
+          scrollTrigger: {
+            markers: true,
+            trigger: box,
+            start: 'bottom bottom',
+            end: 'bottom top',
+            scrub: true
+          }
+        })
+      })
+    }, main) // <- Scope!
+    return () => ctx.revert() // <- Cleanup!
+  }, [])
+
+  return (
+    <div ref={main}>
+      <SliceZone slices={page.data.slices} components={components} />
+    </div>
+  )
 }
 
 export async function getStaticProps({ previewData }) {
--- slices/SingleMediaHighlight/index.js
+++ slices/SingleMediaHighlight/index.js
@@ -14,42 +14,52 @@ const SingleMediaHighlight = ({ slice }) => {
   // slice.variation === 'fullWidthImage'
 
   return (
-    <PrismicLink field={slice.primary.link} target='_blank' className='z-10'>
-      <div
-        className={clsx(getThemeClassNames(slice.primary.theme), 'p-10')}
-        {...(slice.variation === 'fullWidthImage'
-          ? {
-              style: {
-                backgroundImage: `url(${slice.primary.image.url})`,
-                backgroundSize: 'contain',
-                backgroundRepeat: 'no-repeat',
-                aspectRatio: `${slice.primary.image.dimensions.width} / ${slice.primary.image.dimensions.height}`
-              }
-            }
-          : {})}
+    <section className='single-media-highlight'>
+      <PrismicLink
+        field={slice.primary.link}
+        target='_blank'
+        className='block w-full'
       >
-        <span className='sr-only'>{slice.primary.image.alt}</span>
-        <RichText
-          field={slice.primary.title}
-          className='max-w-lg text-4xl font-bold uppercase'
-        />
-        <RichText
-          field={slice.primary.description}
-          className='mt-1 text-lg font-semibold uppercase'
-        />
+        <div
+          className={clsx(
+            getThemeClassNames(slice.primary.theme),
+            'min-h-screen p-10'
+          )}
+          {...(slice.variation === 'fullWidthImage'
+            ? {
+                style: {
+                  backgroundImage: `url(${slice.primary.image.url})`,
+                  backgroundSize: 'contain',
+                  backgroundRepeat: 'no-repeat',
+                  aspectRatio: `${slice.primary.image.dimensions.width} / ${slice.primary.image.dimensions.height}`
+                }
+              }
+            : {})}
+        >
+          <span className='sr-only'>{slice.primary.image.alt}</span>
+          <RichText
+            field={slice.primary.title}
+            className='max-w-lg text-4xl font-bold uppercase'
+          />
+          <RichText
+            field={slice.primary.description}
+            className='mt-1 text-lg font-semibold uppercase'
+          />
 
-        {slice.variation !== 'fullWidthImage' ? (
-          <div className='p-24'>
-            <div className='relative'>
-              <PrismicNextImage
-                field={slice?.primary?.image}
-                className='object-cover'
-              />
+          {slice.variation !== 'fullWidthImage' ? (
+            <div className='p-24'>
+              <div className='relative'>
+                <PrismicNextImage
+                  field={slice?.primary?.image}
+                  className='object-cover '
+                  style={{ filter: `blur(var(--blurIntensity))` }}
+                />
+              </div>
             </div>
-          </div>
-        ) : null}
-      </div>
-    </PrismicLink>
+          ) : null}
+        </div>
+      </PrismicLink>
+    </section>
   )
 }
 
