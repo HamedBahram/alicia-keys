67995200f052fc48170155fd07f19786ff9f3496
Hamed Bahram <hamed.bahram@gmail.com>
2023-03-29 09:19:39 -0400

    remove layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,15 +3,12 @@ import Link from 'next/link'
 import { PrismicProvider } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
 import { repositoryName } from '../prismicio'
-import Layout from '@/components/Layout'
 
 export default function App({ Component, pageProps }) {
   return (
     <PrismicProvider internalLinkComponent={props => <Link {...props} />}>
       <PrismicPreview repositoryName={repositoryName}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Component {...pageProps} />
       </PrismicPreview>
     </PrismicProvider>
   )
